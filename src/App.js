77f26f6be9bd6c087d08ba165da13c73e9cbc6a3
GuidoOlivero <guidooliver2002@gmail.com>
2023-07-03 17:13:09 -0300

    Se crearon nuevos archivos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Heading } from './components/Heading';
+import { UnsplashImage } from './components/UnsplashImage';
+import { Loader } from './components/Loader';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { Search } from './components/Search';
+import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components';
+
+
+// Style
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: sans-serif;
+  }
+`;
+
+const WrapperImages = styled.section`
+  max-width: 70rem;
+  margin: 4rem auto;
+  display: grid;
+  grid-gap: 1em;
+  grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+  grid-auto-rows: 300px;
+`;
 
 function App() {
+  const [images, setImage] = useState([]);
+  const [searchResults, setSearchResults] = useState([]);
+  const apiRoot = "https://api.unsplash.com";
+  const accessKey = '3gP71brc5stZTgliOdZEDgfAVQ-zQO_vQXinqHonSaE'
+
+  useEffect(() => {
+    fetchImages();
+  }, [])
+
+  const fetchImages = (count = 10) => {
+
+    fetch(`${apiRoot}/photos/random?client_id=${accessKey}&count=${count}`)
+      .then(response => response.json())
+      .then(data => {
+        const fetchedImages = data.map(image => ({
+          id: image.id,
+          url: image.urls.thumb,
+          description: image.alt_description,
+          exif: image.exif.name,
+          location: image.location.city,
+        }));
+
+        setImage(prevImages => [...prevImages, ...fetchedImages]);
+        // setIsFetching(false);
+      })
+      .catch(error => {
+        console.log('Error:', error);
+        // setIsFetching(false);
+      });
+  }
+
+
+  const handleSearch = (query) => {
+
+    setImage([]);
+    setSearchResults([]);
+
+    if (query.trim() === '') {
+      fetchImages();
+      return;
+    }
+
+    fetch(`${apiRoot}/search/photos?query=${query}&client_id=${accessKey}`)
+      .then((res) => res.json())
+      .then((data) => {
+        const results = data.results.map((result) => ({
+          id: result.id,
+          url: result.urls.thumb,
+          description: result.alt_description,
+          exif: result.exif.name,
+          location: result.location.city,
+        }));
+
+        setSearchResults(results)
+      })
+      .catch((error) => {
+        console.log('error', error)
+      })
+  }
+
+  const nextPage = () => {
+    fetchImages();
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Heading />
+      <GlobalStyle />
+      <Search onSearch={handleSearch} />
+      <InfiniteScroll
+        dataLength={images.length}
+        next={nextPage}
+        hasMore={true}
+        loader={<Loader />}
+      >
+        <WrapperImages>
+          {searchResults.length > 0
+            ? searchResults.map((image) => (
+              <UnsplashImage
+                key={image.id}
+                url={image.url}
+                description={image.description}
+                exif={image.exif}
+                location={image.location}
+              />
+            ))
+            : images.map((image) => (
+              <UnsplashImage
+                key={image.id}
+                url={image.url}
+                description={image.description}
+                exif={image.exif}
+                location={image.location}
+              />
+            ))}
+
+
+        </WrapperImages>
+      </InfiniteScroll>
     </div>
   );
 }
